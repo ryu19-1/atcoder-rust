96f9f258be77a3f80a19abd0d935ada15897cade
ryu19 <ryu1007kami@gmail.com>
2022-03-17 00:50:47 +0900

    add abc243-d

--- abc243/src/bin/d.rs
+++ abc243/src/bin/d.rs
@@ -1,7 +1,36 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-  input!{
-    n: i32,
+  input! {
+    _n: usize,
+    x: u128,
+    s: Chars,
   };
+  let mut stack = Vec::new();
+  // stackの一番上がLorRと今追加するものがUなら削除
+  for t in s {
+    if stack.is_empty() {
+      stack.push(t);
+    } else if t == 'U' && *stack.last().unwrap() != 'U' {
+      stack.pop();
+    } else {
+      stack.push(t);
+    }
+    // println!("{:?}", stack);
+  }
+
+  let mut ans = x;
+  for ss in stack {
+    match ss {
+      'L' => ans *= 2,
+      'R' => ans = ans * 2 + 1,
+      _ => {
+        if ans > 1 {
+          ans /= 2
+        }
+      }
+    }
+  }
+  println!("{}", ans);
 }
